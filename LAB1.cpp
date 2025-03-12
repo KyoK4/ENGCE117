8396738d3f8424561483718586e592de795f9643
Kanchitpong RMUTL <kanchitpong_sa67@live.rmutl.ac.th>
2025-03-12 15:19:34 +0700

    Update LAB1.cpp

--- LAB1.cpp
+++ LAB1.cpp
@@ -1,47 +1,38 @@
 #include <stdio.h>
 
-int* GetMatrix(int *row, int *col);
+void GetMatrix(int **value, int *row, int *col);
+void PrintMatrix(int *value, int row, int col);
 
 int main() {
-    int *data, m, n;
-    data = GetMatrix(&m, &n);
+    int *data;
+    int m = 3, n = 3;
 
-    if (data == 0) {
-        printf( "Matrix creation failed.\n" );
-    } else {
-        printf( "--------------------------" );
-        printf("\nMatrix:\n");
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                printf("%d ", data[i * n + j]);
-            }
-        printf("\n");
-        }
-    }
+    data = new int[m * n];
+    GetMatrix(&data, &m, &n);
+    PrintMatrix(data, m, n);
+
+    delete[] data;
     return 0;
 }
 
-int* GetMatrix(int *row, int *col) {
-    printf( "Enter number of rows and columns: " );
-    scanf( "%d %d", row, col );
+void GetMatrix(int **value, int *row, int *col) {
+    printf("Enter rows and cols: ");
+    scanf("%d %d", row, col);
 
-    if ( *row < 0 || *col < 0 ) {
-        printf( "Error\n" ) ;
-        return 0;
-    }
-    if ( *row == 0 || *col == 0 ) {
-        printf( "Error\n" ) ;
-        return 0;
-    }
-    
-    int *matrix = new int[(*row) * (*col)]; 
+    *value = new int[*row * *col];
 
-    printf("Enter matrix elements:\n");
-    for (int i = 0; i < *row; i++) {
-        for (int j = 0; j < *col; j++) {
-            scanf("%d", &matrix[i * (*col) + j]);
+    printf("Enter matrix values:\n");
+    for (int i = 0; i < (*row) * (*col); i++) {
+        scanf("%d", &(*value)[i]);
+    }
+}
+
+void PrintMatrix(int *value, int row, int col) {
+    printf("Matrix (%dx%d):\n", row, col);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("%d ", value[i * col + j]);
         }
+        printf("\n");
     }
-
-    return matrix;
 }
